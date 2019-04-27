4d8eb231bfc9bd1bd6aca357215406c90fe803d1
Tiiiger <zty27x@gmail.com>
2019-04-27 14:39:14 -0400

    debug cli script.

--- cli/score.py
+++ cli/score.py
@@ -16,13 +16,13 @@ def main():
     parser = argparse.ArgumentParser('Calculate BERTScore')
     parser.add_argument('--bert', default='bert-base-multilingual-cased',
                         choices=bert_score.bert_types, help='BERT model name (default: bert-base-uncased)')
-    parser.add_argument('-l', '--num_layers', default=8, help='use first N layer in BERT (default: 8)')
-    parser.add_argument('-b', '--batch_size', default=64, help='batch size (default: 64)')
+    parser.add_argument('-l', '--num_layers', type=int, default=8, help='use first N layer in BERT (default: 8)')
+    parser.add_argument('-b', '--batch_size', type=int, default=64, help='batch size (default: 64)')
     parser.add_argument('--no_idf', action='store_true', help='BERT Score without IDF scaling')
     parser.add_argument('-s', '--seg_level', action='store_true', help='show individual score of each pair')
     parser.add_argument('-v', '--verbose', action='store_true', help='increase output verbosity')
-    parser.add_argument('-r', '--ref', required=True, help='reference file path or a string')
-    parser.add_argument('-c', '--cand', required=True,help='candidate (system outputs) file path or a string')
+    parser.add_argument('-r', '--ref', type=str, required=True, help='reference file path or a string')
+    parser.add_argument('-c', '--cand', type=str, required=True, help='candidate (system outputs) file path or a string')
 
     args = parser.parse_args()
 
@@ -45,13 +45,12 @@ def main():
     P = avg_scores[0].cpu().item()
     R = avg_scores[1].cpu().item()
     F1 = avg_scores[2].cpu().item()
-    if args.verbose:
-        print('done in {:.2f} seconds'.format(time.perf_counter() - start))
     msg = '{}_L{}{}_version={} BERT-P: {:.6f} BERT-R: {:.6f} BERT-F1: {:.6f}'.format(
         args.bert, args.num_layers, '_no-idf' if args.no_idf else '', VERSION, P, R, F1)
     print(msg)
     if args.seg_level:
-        for p, r, f in all_preds.tolist():
+        ps, rs, fs = all_preds
+        for p, r, f in zip(ps, rs, fs):
             print('{:.6f}\t{:.6f}\t{:.6f}'.format(p, r, f))
 
 
