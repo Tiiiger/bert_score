d060d7c927679f3ca2ffb3379aadf0d44d96fbd4
Varsha Kishore <vk352@graphite.coecis.cornell.edu>
2019-09-30 16:37:10 -0400

    updated plot function

--- bert_score/score.py
+++ bert_score/score.py
@@ -104,82 +104,82 @@ def score(cands, refs, model_type=None, num_layers=None, verbose=False,
         return P, R, F1
 
 # Under Construction
-def plot_example(h, r, verbose=False, bert="bert-base-multilingual-cased",
-                 num_layers=8, fname=''):
-    pass
-#     """
-#     BERTScore metric.
+def plot_example(candidate, reference, model_type='bert-base-uncased', num_layers=None, fname='', lang=None):
+    
+    assert lang is not None or model_type is not None, \
+        'Either lang or model_type should be specified'
 
-#     Args:
-#         - :param: `h` (str): a candidate sentence
-#         - :param: `r` (str): a reference sentence
-#         - :param: `verbose` (bool): turn on intermediate status update
-#         - :param: `bert` (str): bert specification
-#         - :param: `num_layers` (int): the layer of representation to use
-#     """
-#     assert bert in bert_types
+    if model_type is None:
+        lang = lang.lower()
+        model_type = lang2model[lang]
+    if num_layers is None:
+        num_layers = model2layers[model_type]
 
-#     if verbose:
-#         print('loading BERT model...')
-#     tokenizer = BertTokenizer.from_pretrained(bert)
-#     model = BertModel.from_pretrained(bert)
-#     model.eval()
-#     device = 'cuda' if torch.cuda.is_available() else 'cpu'
-#     model.to(device)
+    assert model_type in model_types
+    tokenizer = AutoTokenizer.from_pretrained(model_type)
+    model = AutoModel.from_pretrained(model_type)
+    model.eval()
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    model.to(device)
 
-#     h_tokens = ['[CLS]'] + tokenizer.tokenize(h) + ['[SEP]']
-#     r_tokens = ['[CLS]'] + tokenizer.tokenize(r) + ['[SEP]']
+    # drop unused layers
+    if 'bert' == model_type[:4] or 'roberta' == model_type[:7]:
+        model.encoder.layer =\
+            torch.nn.ModuleList([layer for layer in model.encoder.layer[:num_layers]])
+    elif 'xlnet' == model_type[:5]:
+        model.layer =\
+            torch.nn.ModuleList([layer for layer in model.layer[:num_layers]])
+    elif 'xlm' in model_type[:3]:
+        model.n_layers = num_layers
+    else:
+        raise ValueError("Not supported")
 
-#     model.encoder.layer = torch.nn.ModuleList([layer for layer in model.encoder.layer[:num_layers]])
-#     idf_dict = defaultdict(lambda: 1.)
+    h_tokens = ['[CLS]'] + tokenizer.tokenize(candidate[0]) + ['[SEP]']
+    r_tokens = ['[CLS]'] + tokenizer.tokenize(reference[0]) + ['[SEP]']
 
-#     ref_embedding, ref_lens, ref_masks, padded_idf = get_bert_embedding([r], model, tokenizer, idf_dict,
-#                                        device=device)
-#     hyp_embedding, ref_lens, ref_masks, padded_idf = get_bert_embedding([h], model, tokenizer, idf_dict,
-#                                        device=device)
+    idf_dict = defaultdict(lambda: 1.)
+    # set idf for [SEP] and [CLS] to 0
+    idf_dict[tokenizer.sep_token_id] = 0
+    idf_dict[tokenizer.cls_token_id] = 0
+    hyp_embedding, masks, padded_idf = get_bert_embedding(candidate, model, tokenizer, idf_dict,
+                                                         device=device, all_layers=False)
+    ref_embedding, masks, padded_idf = get_bert_embedding(reference, model, tokenizer, idf_dict,
+                                                         device=device, all_layers=False)
+    ref_embedding.div_(torch.norm(ref_embedding, dim=-1).unsqueeze(-1))
+    hyp_embedding.div_(torch.norm(hyp_embedding, dim=-1).unsqueeze(-1))
+    sim = torch.bmm(hyp_embedding, ref_embedding.transpose(1, 2))
+    sim = sim.squeeze(0).cpu().numpy()
 
-#     ref_embedding.div_(torch.norm(ref_embedding, dim=-1).unsqueeze(-1))
-#     hyp_embedding.div_(torch.norm(hyp_embedding, dim=-1).unsqueeze(-1))
+    # remove [CLS] and [SEP] tokens 
+    r_tokens = r_tokens[1:-1]
+    h_tokens = h_tokens[1:-1]
+    sim = sim[1:-1,1:-1]
 
-#     batch_size = ref_embedding.size(1)
+    fig, ax = plt.subplots(figsize=(len(r_tokens)*0.8, len(h_tokens)*0.8))
+    im = ax.imshow(sim, cmap='Blues')
 
-#     sim = torch.bmm(hyp_embedding, ref_embedding.transpose(1, 2)).cpu()
-#     sim = sim.squeeze(0).numpy()
+    # We want to show all ticks...
+    ax.set_xticks(np.arange(len(r_tokens)))
+    ax.set_yticks(np.arange(len(h_tokens)))
+    # ... and label them with the respective list entries
+    ax.set_xticklabels(r_tokens, fontsize=10)
+    ax.set_yticklabels(h_tokens, fontsize=10)
+    plt.xlabel("Refernce (tokenized)", fontsize=14)
+    plt.ylabel("Candidate (tokenized)", fontsize=14)
+    plt.title("Similarity Matrix", fontsize=14)
+    
+    # Rotate the tick labels and set their alignment.
+    plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
+             rotation_mode="anchor")
 
-#     # remove [CLS] and [SEP] tokens 
-#     r_tokens = r_tokens[1:-1]
-#     h_tokens = h_tokens[1:-1]
-#     sim = sim[1:-1,1:-1]
+    # Loop over data dimensions and create text annotations.
+    for i in range(len(h_tokens)):
+        for j in range(len(r_tokens)):
+            text = ax.text(j, i, '{:.3f}'.format(sim[i, j]),
+                           ha="center", va="center", color="k" if sim[i, j] < 0.6 else "w")
 
-#     fig, ax = plt.subplots(figsize=(len(r_tokens)*0.8, len(h_tokens)*0.8))
-#     im = ax.imshow(sim, cmap='Blues')
-
-#     # We want to show all ticks...
-#     ax.set_xticks(np.arange(len(r_tokens)))
-#     ax.set_yticks(np.arange(len(h_tokens)))
-#     # ... and label them with the respective list entries
-#     ax.set_xticklabels(r_tokens, fontsize=10)
-#     ax.set_yticklabels(h_tokens, fontsize=10)
-#     plt.xlabel("Refernce", fontsize=10)
-#     plt.ylabel("Candidate", fontsize=10)
-
-#     # Rotate the tick labels and set their alignment.
-#     plt.setp(ax.get_xticklabels(), rotation=45, ha="right",
-#              rotation_mode="anchor")
-
-#     # Loop over data dimensions and create text annotations.
-#     for i in range(len(h_tokens)):
-#         for j in range(len(r_tokens)):
-#             text = ax.text(j, i, '{:.3f}'.format(sim[i, j]),
-#                            ha="center", va="center", color="k" if sim[i, j] < 0.6 else "w")
-
-# #     P = sim.max(1).mean()
-# #     R = sim.max(0).mean()
-# #     F1 = 2 * P * R / (P + R)
-
-#     fig.tight_layout()
-# #     plt.title("BERT-F1: {:.3f}".format(F1), fontsize=10)
-#     if fname != "":
-#         print("Saved figure to file: ", fname+".png")
-#         plt.savefig(fname+'.png', dpi=100)
-#     plt.show()
+    fig.tight_layout()
+    if fname != "":
+        print("Saved figure to file: ", fname+".png")
+        plt.savefig(fname+'.png', dpi=100)
+    plt.show()
