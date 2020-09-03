9b4116c6b7f2fc0bb7fafa4d65d7cf13e4524421
Felix Wu <felixgwu@gmail.com>
2020-09-03 15:33:01 -0400

    to v0.3.6

--- bert_score/__init__.py
+++ bert_score/__init__.py
@@ -1,3 +1,3 @@
-__version__ = "0.3.5"
+__version__ = "0.3.6"
 from .score import *
 from .scorer import *
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="bert_score",
-    version='0.3.5',
+    version='0.3.6',
     author="Tianyi Zhang*, Varsha Kishore*, Felix Wu*, Kilian Q. Weinberger, and Yoav Artzi",
     author_email="tzhang@asapp.com",
     description="PyTorch implementation of BERT score",
