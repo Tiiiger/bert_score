c25f99556a43113cff4efca3ddcf42428e1cd3d1
fireindark707 <30530581+fireindark707@users.noreply.github.com>
2022-04-07 00:30:38 +0800

    update downloading url of WMT16 (#127)
    Original link is broke.

--- tune_layers/download_data.sh
+++ tune_layers/download_data.sh
@@ -2,7 +2,7 @@ if ! [ -d wmt16 ]; then
     mkdir wmt16
     gz_file=wmt16-metrics-results.tar.gz
     if ! [ -f $gz_file ]; then
-        wget https://www.computing.dcu.ie/~ygraham/wmt16-metrics-results.tar.gz
+        wget https://www.scss.tcd.ie/~ygraham/wmt16-metrics-results.tar.gz
     fi
     tar -xzf $gz_file -C wmt16
     rm -f $gz_file
